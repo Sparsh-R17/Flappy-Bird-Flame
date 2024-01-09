2a899739dcdaba9e19344a821b4adbfbe4910817
Sparsh-R17 <sparsh170702@gmail.com>
2024-01-09 22:32:39 +0530

    Added: Background

--- lib/flappy_bird_game.dart
+++ lib/flappy_bird_game.dart
@@ -1,5 +1,17 @@
 import 'package:flame/game.dart';
+import 'package:flappy_bird_flame/game/widgets/background.dart';
+import 'package:flappy_bird_flame/game/widgets/ground.dart';
 
 class FlappyBirdGame extends FlameGame {
   FlappyBirdGame();
+
+  @override
+  Future<void> onLoad() async {
+    addAll(
+      [
+        Background(),
+        Ground(),
+      ],
+    );
+  }
 }
